d7e31f4e9ab90c3aa3788e0c688e5f93840b9ddb
Fan Xu - I15 <fan.xu@nordakademie.de>
2018-09-09 22:01:35 +0200

    changed objectPageHeader layout

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -100,11 +100,9 @@ sap.ui.define([
             var history = History.getInstance();
             var previousHash = history.getPreviousHash();
 
-            //if ui5 recorded previous pages, simply go back in history
             if (previousHash !== undefined) {
                 window.history.go(-1);
             } else {
-                //if there is no ui5 history
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("list", {}, true);
             }
