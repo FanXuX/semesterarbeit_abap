e8dd54f3d5731563030145894e15b9a2ce559322
Fan Xu - I15 <fan.xu@nordakademie.de>
2018-09-09 14:35:40 +0200

    filter from edit boxes

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,14 +1,41 @@
+/* global _:true */
 sap.ui.define([
     "de/nak/productlist/controller/BaseController",
     'sap/ui/model/Filter',
-    'sap/ui/model/Sorter'
-], function (BaseController, Sorter) {
+    'sap/ui/model/Sorter',
+    "de/nak/productlist/libs/lodash.min"
+], function (BaseController, Filter, Sorter, lodash) {
     "use strict";
     return BaseController.extend("de.nak.productlist.controller.List", {
 
         _oDialog: null,
 
         onInit: function () {
+
+            const fnGetById = function(id) {
+                return this.getView().byId(id);
+            }.bind(this);
+
+            const fnExtractTextValue = function(edit) {
+                return {
+                    value() {
+                        return edit.getValue();
+                    }
+                };
+            };
+
+            // FIXME
+            this.aKeys = ['Matnr', 'Maktx', 'Stprs'];
+            this.oProductId = fnGetById('eProductId');
+            this.oProductName = fnGetById('eProductName');
+            this.oStdPrice = fnGetById('slStdPrice');
+            this.oTable = fnGetById('idProductsTable');
+
+            this.filterItems = {
+                'Matnr': fnExtractTextValue(this.oProductId),
+                'Maktx': fnExtractTextValue(this.oProductName)
+                //TODO price
+            };
 
             // prepare functions for grouping
             this.mGroupFunctions = {
@@ -63,19 +90,6 @@ sap.ui.define([
         },
 
         /**
-         * Open SortDialog
-         * @param oEvent
-         */
-        handleSortDialogButtonPressed: function (oEvent) {
-            if (!this._oDialog) {
-                this._oDialog = sap.ui.xmlfragment("de.nak.productlist.fragment.SortDialog", this);
-            }
-            // toggle compact style
-            jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialog);
-            this._oDialog.open();
-        },
-
-        /**
          * Execute Sorting with group function
          * @param oEvent
          */
@@ -102,7 +116,74 @@ sap.ui.define([
             aSorters.push(new Sorter(sPath, bDescending));
             oBinding.sort(aSorters);
 
-        }
+        },
+
+        /**
+         * Open SortDialog
+         * @param oEvent
+         */
+        handleSortDialogButtonPressed: function (oEvent) {
+            if (!this._oDialog) {
+                this._oDialog = sap.ui.xmlfragment("de.nak.productlist.fragment.SortDialog", this);
+            }
+            // toggle compact style
+            jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialog);
+            this._oDialog.open();
+        },
+
+        onFilterChange: function() {
+
+            const fnDoFilter = function(filterValueMap){
+
+                const filterList = _.map(filterValueMap, function(v, k) {
+                    return new sap.ui.model.Filter(k, sap.ui.model.FilterOperator.Contains, [v]);
+                });
+
+                this.oTable.getBinding('items').filter(filterList)
+            }.bind(this);
+
+            const fnExtractFilterValues = function(filterItemsMap) {
+                return _.transform(filterItemsMap, function(acc, v, k) {
+                    if (v.value()) {
+                        acc[k] = v.value();
+                    }
+                }, {});
+            }.bind(this);
+
+
+            fnDoFilter(fnExtractFilterValues(this.filterItems));
+            return;
+
+            const aCurrentFilterValues = [];
+
+            const fnFilterTable = function() {
+                const fnGetFilters = function (aCurrentFilterValues) {
+                    return this.aKeys.map(function (sCriteria, i) {
+                        return new sap.ui.model.Filter(sCriteria, sap.ui.model.FilterOperator.Contains, aCurrentFilterValues[i]);
+                    });
+                }.bind(this);
+                const fnGetFilterCriteria = function (aCurrentFilterValues){
+                    return this.aKeys.filter(function (el, i) {
+                        if (aCurrentFilterValues[i] !== "") return  el;
+                    });
+                }.bind(this);
+
+                this.oTable.getBinding('items').filter(fnGetFilters(aCurrentFilterValues));
+
+                // this.updateFilterCriterias(fnGetFilterCriteria(aCurrentFilterValues));
+            }.bind(this);
+
+            const fnGetKey = function(item) {
+                return item.getSelectedItem() ? item.getSelectedItem().getKey() : "";
+            }.bind(this);
+
+            aCurrentFilterValues.push(fnGetKey(this.oProductId));
+            aCurrentFilterValues.push(fnGetKey(this.oProductName));
+            aCurrentFilterValues.push(fnGetKey(this.oStdPrice));
+
+            fnFilterTable(aCurrentFilterValues);
+        },
+
 
 
     })
